a311c68cfa636e463cf48df62ea4fc7c757e1072
unknown <fangy2543@hotmail.com>
2020-11-04 20:56:56 +0700

    commit

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,28 +7,28 @@ import icon from '../img/profile-sq.jpg'
 export const Profile = () => {
     return (
         <div className="profile">
-            
+
             <div className="profile-content">
                 <div className="profile-top">
                     <div className="profile-left">
                         <img src={icon} alt="" className="profile-img" />
                     </div>
                     <div className="profile-right">
-                        <div className = "text-header">
-                            Profile <i class="far fa-id-card"></i> 
+                        <div className="text-header">
+                            Profile <i class="far fa-id-card"></i>
                         </div>
-                        <div className = "text">
-                            <i class="fas fa-user-tag"></i> Name : Nattarat Serirat [ fang ] <br/>
-                            <i class="fas fa-birthday-cake"></i> Age : 20<br/>
-                            <i class='far fa-calendar-check'></i> Birthdate : 22 February 2000 <br/>
-                            <i class="fas fa-graduation-cap"></i> Education : Computer Engineering <br/>
+                        <div className="text">
+                            <i class="fas fa-user-tag"></i> Name : Nattarat Serirat [ fang ] <br />
+                            <i class="fas fa-birthday-cake"></i> Age : 20<br />
+                            <i class='far fa-calendar-check'></i> Birthdate : 22 February 2000 <br />
+                            <i class="fas fa-graduation-cap"></i> Education : Computer Engineering <br />
                             <i class="fas fa-phone"></i> Contact : 080-284-8185
-                                     
-                        </div> 
+
+                        </div>
                     </div>
                 </div>
                 <div className="profile-bot">
-                    <br/>
+                    <br />
                     ใส่ไรดี??
                 </div>
             </div>
